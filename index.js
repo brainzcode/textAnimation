90e90dd28589c22667cf2aa1cced38dcea0480d0
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-02-10 00:00:37 +0100

    JS template

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-let index = 0
+let index = 0,
+	interval = 1000;
 
-const rand = (min, max) => {
-    Math.floor(Math.random() * (min - max + 1)) + min;
-}
+const magic = document.getElementsByClassName('magic-star');
 
+const rand = (min, max) => Math.floor(Math.random() * (min - max + 1)) + min;
 
-const animate = star => {
-    star.style.setProperty(`${rand(0, 100)}%`)
-    
+const animate = (star) => {
+	star.style.setProperty('--star-left', `${rand(0, 100)}%`);
+	star.style.setProperty('--star-top', `${rand(-20, 80)}`);
+
+	star.style.animation = 'none';
+	star.offsetHeight;
+	star.style.animation = '';
+};
+
+for (const star of magic) {
+	setTimeout(() => {
+		animate(star);
+
+		setInterval(() => animate(star), 1000);
+	}, index++ * (interval / 3));
 }

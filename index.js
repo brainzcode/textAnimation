7511ee351a8f6a4ffd9888ed888bd6afc4005ca1
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-02-10 00:05:33 +0100

    minor JS changes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const magic = document.getElementsByClassName('magic-star');
 const rand = (min, max) => Math.floor(Math.random() * (min - max + 1)) + min;
 
 const animate = (star) => {
-	star.style.setProperty('--star-left', `${rand(0, 100)}%`);
-	star.style.setProperty('--star-top', `${rand(-20, 80)}`);
+	star.style.setProperty('--star-left', `${rand(10, 100)}%`);
+	star.style.setProperty('--star-top', `${rand(20, 80)}`);
 
 	star.style.animation = 'none';
 	star.offsetHeight;
